2f694cc4651d60b202b571401cf730fdbab68776
E <work@Es-MacBook-Pro.hsd1.ca.comcast.net>
2021-12-28 15:48:04 -0800

    created the different initial folders for components and structure for simpler code

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+const Navbar = () => {
+    return (
+        <div className='3salaz__navbar'></div>
+    )
+}
+
+export default Navbar
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const WhatGPT3 = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default WhatGPT3;
